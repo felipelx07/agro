80f8864177b272589f9175d8a902d6165a460cdf
itangast <itangast@576ac3d7-c726-0410-b766-b73afa0880d3>
2007-01-12 16:23:20 +0000

    Añadido las carpetas sql y strSQL

---
+++ trunk/strSQL/__init__.py
@@ -0,0 +1,1 @@
+# init para cadenas sql
---
+++ trunk/strSQL/basicas.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# -*- coding: UTF8 	-*-
+import config
+
+strSelectEstanque = """SELECT * FROM """ + config.schema + """.estanque ORDER BY codigo_estanque"""
+
+strSelectTipoFicha = """SELECT * FROM """ + config.schema + """.tipo_ficha ORDER BY codigo_tipo_ficha"""
+
+strSelectInsumo = """SELECT * FROM """ + config.schema + """.insumo ORDER BY codigo_insumo"""
+
+strSelectTipoAplicacion = """SELECT * FROM """ + config.schema + """.tipo_aplicacion ORDER BY codigo_tipo_aplicacion"""
+
+#strSelectTipoControl = """SELECT * FROM """ + config.schema + """.tipo_control ORDER BY codigo_tipo_control"""
+strSelectTipoControl = """ SELECT
+                            s.codigo_tipo_control,
+                            s.descripcion_tipo_control,
+                            s.tipo_resultado,
+                            s.codigo_unidad,
+                            v.descripcion_unidad
+                        FROM """ + config.schema + """.tipo_control s
+                        INNER JOIN """ + config.schema + """.unidad v
+                        ON s.codigo_unidad = v.codigo_unidad
+                        ORDER BY s.descripcion_tipo_control"""
+
+strSelectFicha = """SELECT
+                            f.codigo_tipo_ficha,
+                            t.descripcion_tipo_ficha,
+                            f.descripcion_ficha,
+                            f.rut
+                        FROM """ + config.schema + """.ficha f
+                        INNER JOIN """ + config.schema + """.tipo_ficha t
+                        ON f.codigo_tipo_ficha = t.codigo_tipo_ficha
+                        ORDER BY f.descripcion_ficha"""
+
+strSelectCuartel = """SELECT c.codigo_cuartel,
+                             c.descripcion_cuartel,
+                             s.descripcion_sector,
+                             c.codigo_sector,
+                             c.codigo_variedad,
+                             v.descripcion_variedad
+                      FROM """ + config.schema + """.cuartel c
+                      INNER JOIN """ + config.schema + """.sector s
+                      ON c.codigo_sector = s.codigo_sector
+                      INNER JOIN """ + config.schema + """.variedad v
+                      ON v.codigo_variedad = c.codigo_variedad
+                      ORDER BY c.descripcion_cuartel"""
+
+strSelectSector = """SELECT s.*, f.descripcion_ficha
+                    FROM """ + config.schema + """.sector s
+                    INNER JOIN """ + config.schema + """.ficha f
+                    ON s.rut_propietario = f.rut_ficha
+                    ORDER BY s.descripcion_sector"""
+                    
+strSelectCosecha = """SELECT c.*,
+                            f.descripcion_ficha,
+                            s.descripcion_cuartel,
+                            v.descripcion_variedad
+                        FROM """ + config.schema + """.cosecha c
+                        INNER JOIN """ + config.schema + """.ficha f
+                        ON c.rut_ficha = f.rut_ficha
+                        INNER JOIN """ + config.schema + """.cuartel s
+                        ON c.codigo_cuartel = s.codigo_cuartel
+                        INNER JOIN """ + config.schema + """.variedad v
+                        ON c.codigo_variedad = v.codigo_variedad
+                        ORDER BY c.codigo_cosecha"""
+                        
+strSelectLabor = """SELECT * FROM """ + config.schema + """.labor ORDER BY codigo_labor"""
+
+strSelectMaquinaria = """SELECT * FROM """ + config.schema + """.maquinaria ORDER BY codigo_maquinaria"""
+
+strSelectVariedad = """SELECT * FROM """ + config.schema + """.variedad ORDER BY codigo_variedad"""
+
+strSelectCultivo = """SELECT * FROM """ + config.schema + """.cultivo ORDER BY codigo_cultivo"""
+
+strSelectDetalleRelacion = """SELECT %s FROM %s WHERE %s = %s"""
+
+strSelectUnidad = """SELECT * FROM """ + config.schema + """.unidad ORDER BY descripcion_unidad"""
+
+strSelectProducto = """SELECT * FROM """ + config.schema + """.producto ORDER BY descripcion_producto"""
+
+strSelectTipoDocumento = """SELECT * FROM """ + config.schema + """.tipo_documento ORDER BY codigo_tipo_documento"""
+
+strNextValCodigo = """SELECT nextval('""" + config.schema + """.%s_codigo_%s_seq'::regclass)"""
+
+strSelectDespacho = """SELECT
+                        d.codigo_despacho,
+                        d.codigo_tipo_documento,
+                        t.descripcion_tipo_documento,
+                        d.numero_documento,
+                        d.fecha_despacho,
+                        d.hora_despacho,
+                        d.rut_cliente,
+                        c.descripcion_ficha,
+                        d.rut_transportista,
+                        cc.descripcion_ficha,
+                        d.destino,
+                        d.observaciones,
+                        d.sello_seguridad
+                        FROM """ + config.schema + """.despacho d
+                        INNER JOIN """ + config.schema + """.ficha c
+                        ON d.rut_cliente = c.rut_ficha
+                        join """ + config.schema + """.ficha cc
+                        on d.rut_transportista = cc.rut_ficha
+                        INNER JOIN """ + config.schema + """.tipo_documento t
+                        ON d.codigo_tipo_documento = t.codigo_tipo_documento
+                        ORDER BY d.codigo_despacho"""
+                        
+                        
+strSelectTipoDespacho = """SELECT d.%s,
+                                m.%s,
+                                d.cantidad
+                                FROM %s d
+                                JOIN %s e
+                                ON e.%s = d.%s
+                                JOIN %s m
+                                on d.%s = m.%s
+                                where %s = %s"""
+                                
+                                
+
+strSelectMovimientoEstanque = """select fecha, porcentaje, volumen, movimiento, desce_a, desce_b
+                            from """ + config.schema + """.vw_movimiento_estanque_fecha order by fecha"""
